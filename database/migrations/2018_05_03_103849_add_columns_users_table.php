254cab36be9eb10d3495fe31e910ef1ec8102723
nitaking <fsrgrow@gmail.com>
2018-05-03 20:33:28 +0900

    [ADD]USERS MIGRATION email_verifyカラム追加

---
+++ database/migrations/2018_05_03_103849_add_columns_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnsUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->tinyInteger('email_verified')->default(0);
+            $table->string('email_verify_token')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('email_verified');
+            $table->dropColumn('email_verify_token');
+        });
+    }
+}
