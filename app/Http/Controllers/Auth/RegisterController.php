3565813f647fed9b8ff0935a600192123f91cd73
nitaking <fsrgrow@gmail.com>
2018-05-04 02:14:40 +0900

    [DELETE]UUID.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -80,11 +80,6 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-//        return User::create([
-//            'name' => $data['name'],
-//            'email' => $data['email'],
-//            'password' => Hash::make($data['password']),
-//        ]);
 
         $user = User::create([
             'email' => $data['email'],
